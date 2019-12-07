e66feb9ea809b35eea5e531198b514ff71630ca8
Peter Luber <prluber@gmail.com>
2019-12-06 22:39:35 -0800

    nothing

--- src/App.js
+++ src/App.js
@@ -2,25 +2,63 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: [],
+    inputText: ""
+  }
+
+  handleChange = (event) => {
+    this.setState({inputText: event.target.value})
+  }
+
+  handleSubmit = (event) => {
+    this.setState({
+      todos: [...this.state.todos, this.state.inputText],
+      inputText: ""
+    
+    })
+    event.preventDefault()
+  }
+
+  listWeed = () => {
+    return (
+      <ul>
+        
+      </ul>
+    )
+
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Smoke weed every day
+          </p>
+        </header>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            weed   
+            <input 
+              type="text" 
+              onChange={this.handleChange}
+              value={this.state.inputText}
+              /> 
+          </label>
+          
+        </form>
+        <ul>
+         {this.state.todos.map(function(todo) {
+           return <li>{todo}</li>
+         })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
