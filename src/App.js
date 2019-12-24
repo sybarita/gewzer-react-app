7bb94bdfcbe0018ac6fea0385d50b869599e0e5f
Peter Luber <prluber@gmail.com>
2019-12-23 23:47:59 -0800

    title fix

--- src/App.js
+++ src/App.js
@@ -39,9 +39,10 @@ class App extends React.Component {
         <img src={logo} className="App-logo" alt="logo" />
         <div id="bioBox">
           juan escobedo aka 'gewzer'<br></br> 
-          @gewzer38 on Instagram <br></br>
-          @juander on <a href="http://soundcloud.com/juander">SoundCloud</a> <br></br>
-          los angeles / fresno
+          @gewzer38 on Instagram<br></br>
+          @juander on <a href="http://soundcloud.com/juander">soundcloud</a><br></br>
+          los angeles / fresno<br></br>
+          3800
         </div>
         <form id="form1" onSubmit={this.handleSubmit}>
           <label>
